7f8f74e9eed6c94d5994a6b5d095d3400eadb20b
Leofront-end <ls171258@gmail.com>
2025-04-07 20:51:19 -0300

    Cor muda

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ let meio = 0
 
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
 
+let liArray = []
 let baixo = 0
 let alto = numeros.length - 1
-let li
+var li
 
 function lista(chute){
     let lista = document.createElement("li")
@@ -39,6 +40,16 @@ while (baixo <= alto) {
 
     esse.addEventListener('click', () => {
         li = lista(chute)
+        liArray.push(li)
+
+        liArray.forEach(e => {
+            if (e == liArray[liArray.length-1]){
+                console.log(e)
+                e.classList.add("ativo")
+            } else {
+                e.classList.remove("ativo")
+            }
+        })
         
     })
 
@@ -50,6 +61,17 @@ while (baixo <= alto) {
         titulo.textContent = `Seu número é ${chute}?`
 
         li = lista(chute)  
+        liArray.push(li)
+
+        liArray.forEach(e => {
+            if (e == liArray[liArray.length-1]){
+                console.log(e)
+                e.classList.add("ativo")
+            } else {
+
+                e.classList.remove("ativo")
+            }
+        })
         if (li.textContent == 1){
             menor.setAttribute("disabled","")
             alert("Chegou no inicio da lista")
@@ -65,6 +87,18 @@ while (baixo <= alto) {
         titulo.textContent = `Seu número é ${chute}?`
 
         li = lista(chute)
+        liArray.push(li)
+
+        liArray.forEach(e => {
+            if (e == liArray[liArray.length-1]){
+                console.log(e)
+                e.classList.add("ativo")
+            } else {
+
+                e.classList.remove("ativo")
+            }
+        })
+
    
 
         if (li.textContent == 100){
